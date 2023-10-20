6e28bad00dd31a1af3c6f49d355cc4d88b9fc8f1
Gabriel Santos <gabrielsantos.sgra@gmail.com>
2023-10-19 22:10:16 -0300

    header alocado em headerjs

---
+++ src/Header/header.js
@@ -0,0 +1,15 @@
+function header() {
+  headerElement.innerHTML = ` <header class="card-grs bg-dark text-white">
+    <nav class="nav-items">
+        <a class="button-grs to-left padding-large" href="#INICIO"><strong><img class="logo"
+                    src="./src/images/svg20.png" alt=""></strong></a>
+        <a class="button-grs to-right padding-large" href="#contato">CONTATO</a>
+        <a class="button-grs to-right padding-large" href="#artigos">ARTIGOS</a>
+        <a class="button-grs to-right padding-large" href="#projetos">PROJETOS</a>
+        <a class="button-grs to-right padding-large" href="#sobre">SOBRE</a>
+
+    </nav>
+</header>`;
+}
+
+header();
